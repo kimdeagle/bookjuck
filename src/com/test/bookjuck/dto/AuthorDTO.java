c6b5d55c5be47e8cdf185df6be32275fe27a9872
terran0526 <terran0526@naver.com>
2021-02-22 14:16:14 +0900

    DTO create
    DTO 추가
     - 도서정보
     - E-Book정보
     - 카테고리(대, 중, 소)
     - 작가정보

---
+++ src/com/test/bookjuck/dto/AuthorDTO.java
@@ -0,0 +1,29 @@
+package com.test.bookjuck.dto;
+
+public class AuthorDTO {
+
+	private String seq;
+	private String name;
+	private String intro;
+	
+	public String getSeq() {
+		return seq;
+	}
+	public void setSeq(String seq) {
+		this.seq = seq;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getIntro() {
+		return intro;
+	}
+	public void setIntro(String intro) {
+		this.intro = intro;
+	}
+	
+	
+}
