2a4e87c5fd8476a92dc3e3b022e83eb30740d893
terran0526 <terran0526@naver.com>
2021-02-22 15:29:13 +0900

    DAO create
    DAO 추가
     - BookDAO
     - EBookDAO
     - AuthorDAO
     - CategoryDAO(L, M, S)

---
+++ src/com/test/bookjuck/dao/AuthorDAO.java
@@ -0,0 +1,34 @@
+package com.test.bookjuck.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import com.test.bookjuck.DBUtil;
+
+public class AuthorDAO {
+
+	   private Connection conn;
+	   private Statement stat;
+	   private PreparedStatement pstat;
+	   private ResultSet rs;
+
+	   public AuthorDAO() {
+	      // DB 연결
+	      conn = DBUtil.open();
+	   }
+
+	   public void close() {
+	      
+	      try {
+	         
+	         conn.close();
+
+	      } catch (Exception e) {
+	         System.out.println(e);
+	      }
+
+	   }
+	
+}
