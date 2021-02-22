1c7008478bc58b74ad3414b8336f6aa5360b22ef
abcdana <74163296+abcdana@users.noreply.github.com>
2021-02-22 14:45:23 +0900

    교/취/환+중고거래 DTO추가

---
+++ src/com/test/bookjuck/dto/BookRefundAcctDTO.java
@@ -0,0 +1,53 @@
+package com.test.bookjuck.dto;
+
+public class BookRefundAcctDTO {
+	
+	private String seq;
+	private String seqBookRefund; 
+	private String seqBookCancel;
+	private String acctHolder;
+	private String bank;
+	private String acctNum;
+	
+	
+	public String getSeq() {
+		return seq;
+	}
+	public void setSeq(String seq) {
+		this.seq = seq;
+	}
+	public String getSeqBookRefund() {
+		return seqBookRefund;
+	}
+	public void setSeqBookRefund(String seqBookRefund) {
+		this.seqBookRefund = seqBookRefund;
+	}
+	public String getSeqBookCancel() {
+		return seqBookCancel;
+	}
+	public void setSeqBookCancel(String seqBookCancel) {
+		this.seqBookCancel = seqBookCancel;
+	}
+	public String getAcctHolder() {
+		return acctHolder;
+	}
+	public void setAcctHolder(String acctHolder) {
+		this.acctHolder = acctHolder;
+	}
+	public String getBank() {
+		return bank;
+	}
+	public void setBank(String bank) {
+		this.bank = bank;
+	}
+	public String getAcctNum() {
+		return acctNum;
+	}
+	public void setAcctNum(String acctNum) {
+		this.acctNum = acctNum;
+	}
+	
+	
+	
+
+}
---
+++ src/com/test/bookjuck/dto/NewDeliveryDTO.java
@@ -0,0 +1,59 @@
+package com.test.bookjuck.dto;
+
+public class NewDeliveryDTO {
+	
+	private String seq;
+	private String seqBookChange;
+	private String name;
+	private String address;
+	private String tel;
+	private String deliveryCompany;
+	private String deliveryNumber;
+	
+	
+	public String getSeq() {
+		return seq;
+	}
+	public void setSeq(String seq) {
+		this.seq = seq;
+	}
+	public String getSeqBookChange() {
+		return seqBookChange;
+	}
+	public void setSeqBookChange(String seqBookChange) {
+		this.seqBookChange = seqBookChange;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getDeliveryCompany() {
+		return deliveryCompany;
+	}
+	public void setDeliveryCompany(String deliveryCompany) {
+		this.deliveryCompany = deliveryCompany;
+	}
+	public String getDeliveryNumber() {
+		return deliveryNumber;
+	}
+	public void setDeliveryNumber(String deliveryNumber) {
+		this.deliveryNumber = deliveryNumber;
+	}
+	
+	
+
+}
