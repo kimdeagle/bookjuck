ac9f700f984bb7d90079fdb53bd86991410cffc4
tnrud430 <68557310+tnrud430@users.noreply.github.com>
2021-02-22 16:16:53 +0900

    DAO 추가

---
+++ src/com/test/bookjuck/dao/BookOrderDAO.java
@@ -0,0 +1,35 @@
+package com.test.bookjuck.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import com.test.bookjuck.DBUtil;
+
+public class BookOrderDAO {
+	
+
+	private Connection conn;
+	private Statement stat;
+	private PreparedStatement pstat;
+	private ResultSet rs;
+
+	public BookOrderDAO() {
+		// DB 연결
+		conn = DBUtil.open();
+	}
+
+	public void close() {
+      
+		try {
+         
+			conn.close();
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+   }
+
+}
